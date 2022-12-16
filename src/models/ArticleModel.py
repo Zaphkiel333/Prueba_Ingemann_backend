051b20dfae78403659fd24936738346e7c8fd670
Rafael Hernandez <rhhs333@gmail.com>
2022-12-16 06:11:31 -0600

    obtencion de un articulo a la vez

--- src/models/ArticleModel.py
+++ src/models/ArticleModel.py
@@ -2,9 +2,9 @@ from .entities.Article import Article
 from ..database.db import get_connection
 
 
-class ArticleModel():
+class ArticleModel:
     @classmethod
-    def get_article(self):
+    def get_articles(self):
 
         connection = None
 
@@ -22,6 +22,31 @@ class ArticleModel():
                     article = Article(row[0], row[1], row[2], row[3], row[4], row[5])
                     articles.append(article.to_JSON())
             return articles
+        except Exception as ex:
+            raise Exception(ex)
+        finally:
+            connection.close()
+
+    @classmethod
+    def get_article(self, codigo):
+        connection = None
+
+        try:
+            connection = get_connection()
+
+            with connection.cursor() as cursor:
+                query = """SELECT *
+                            FROM articulo
+                            WHERE codigo = %s"""
+                cursor.execute(query, (codigo,))
+                row = cursor.fetchone()
+
+                article = None
+                if row is not None:
+                    article = Article(row[0], row[1], row[2], row[3], row[4], row[5])
+                    article = article.to_JSON()
+
+            return article
         except Exception as ex:
             raise Exception(ex)
         finally:
--- src/routes/Article.py
+++ src/routes/Article.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
 
 from src.models.ArticleModel import ArticleModel
 
@@ -6,9 +6,21 @@ main = Blueprint('article_blueprint', __name__)
 
 
 @main.route('/')
-def get_article():
+def get_articles():
     try:
-        articles = ArticleModel.get_article()
-        return jsonify(articles)
+        articles = ArticleModel.get_articles()
+        return articles
     except Exception as ex:
-        return jsonify({'error': str(ex)}), 500
+        return {'error': str(ex)}, 500
+
+
+@main.route('/<codigo>')
+def get_article(codigo):
+    try:
+        article = ArticleModel.get_article(codigo)
+        if article is not None:
+            return article
+        else:
+            return {}, 404
+    except Exception as ex:
+        return {'error': str(ex)}, 500
